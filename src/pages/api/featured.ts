42c00ce7258e92112d180191ae306792bab145e8
pipe <feliperarnau@gmail.com>
2025-01-13 15:07:03 -0300

    felipes-change

--- src/pages/api/featured.ts
+++ src/pages/api/featured.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const FEATURED_FILM =
+export const FEATURED_FILM =
   "https://api.themoviedb.org/3/movie/now_playing?api_key=6f26fd536dd6192ec8a57e94141f8b20";
 
 type Movie = {
@@ -10,7 +10,7 @@ type Movie = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Movie>,
+  res: NextApiResponse<Movie>
 ) {
   // Your api fetching code here
   res.status(200).json({ name: "Moana" });
--- src/pages/movies.tsx
+++ src/pages/movies.tsx
@@ -1,25 +1,52 @@
 import { prisma } from "@src/lib/prisma";
 import type { Movie } from "@prisma/client";
+import { FEATURED_FILM } from "@src/lib/endpoints";
+import "./movies.css";
 
 // This allows us to visualize movies in the database
 
 export default function Movies({ movies }: { movies: Movie[] }) {
   return (
-    <div>
+    <div className="wrapper">
       <h1 className="h1"> Movies </h1>
       <br />
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            {movie.name} - {movie.image_url}
-          </li>
-        ))}
-      </ul>
+      {movies.map((movie) => (
+        <div key={movie.id} className="movie">
+          <h4>{movie.name}</h4>
+          <div>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.image_url}`}
+              alt={movie.name}
+            />
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
 
 export async function getServerSideProps() {
+  // const res = await fetch(FEATURED_FILM);
+  // const data = await res.json();
+
+  // // console.log(data.results);
+
+  // const movieArray: any[] = [];
+
+  // if (data.results && data.results.length)
+  //   data.results.forEach((movie: any, index: number) => {
+  //     movieArray.push({
+  //       // id: index,
+  //       name: movie.title,
+  //       image_url: movie.backdrop_path,
+  //       vote_average: movie.vote_average,
+  //     });
+  //   });
+
+  // if (movieArray.length) {
+  //   const store = await prisma.movie.createMany({ data: movieArray });
+  // }
+
   const movies = await prisma.movie.findMany();
 
   return {
